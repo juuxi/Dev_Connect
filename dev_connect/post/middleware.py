5307789162261524f7019b183c650e8828d553c6
juuxi <dimashopyrev@mail.ru>
2026-08-29 11:14:41 +0300

    refactor logging middleware logic to prevent race conditions

--- dev_connect/post/middleware.py
+++ dev_connect/post/middleware.py
@@ -1,17 +1,21 @@
-from django.utils.deprecation import MiddlewareMixin
 from time import time
 import logging
 
 
-class LoggingMiddleware(MiddlewareMixin):
-    logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
-    def process_request(self, request):
-        self.start = time()
 
-    def process_response(self, request, response):
+class LoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        start_time = time()
+        response = self.get_response(request)
+        duration = round(time() - start_time, 5)
+
         log_info = 'requesting user: '
         log_info += request.user.username if request.user else 'Anonymous user'
-        log_info += f', time to process request: {round(time() - self.start, 5)}s'
-        self.logger.info(log_info)
+        log_info += f', time to process request: {duration}s'
+        logger.info(log_info)
         return response
